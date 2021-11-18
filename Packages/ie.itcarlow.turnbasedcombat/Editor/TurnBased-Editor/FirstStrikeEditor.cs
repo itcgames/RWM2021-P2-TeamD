ea8e79c2b5398deca92790d0f94fbfe4b34ac68f
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-11-18 13:17:38 +0000

    integrated component: turn-based combat

---
+++ Packages/ie.itcarlow.turnbasedcombat/Editor/TurnBased-Editor/FirstStrikeEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FirstStrikeChance)), CanEditMultipleObjects]
+public class FirstStrikeEditor : Editor
+{
+    public SerializedProperty
+          type_prop,
+          onAdvantage_prop,
+          attribute_prop,
+          successThreshold_prop;
+
+    private void OnEnable()
+    {
+        type_prop = serializedObject.FindProperty("m_type");
+        onAdvantage_prop = serializedObject.FindProperty("m_onAdvantage");
+        attribute_prop = serializedObject.FindProperty("m_attribute");
+        successThreshold_prop = serializedObject.FindProperty("m_successThreshold");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(type_prop);
+
+        FirstStrikeChance.CheckType type = (FirstStrikeChance.CheckType)type_prop.enumValueIndex;
+
+        switch (type)
+        {
+            case FirstStrikeChance.CheckType.BooleanExpressions:
+                EditorGUILayout.PropertyField(onAdvantage_prop, new GUIContent("onAdvantage"));
+                break;
+            case FirstStrikeChance.CheckType.Random:
+                EditorGUILayout.PropertyField(successThreshold_prop, new GUIContent("successThreshold"));
+                break;
+            case FirstStrikeChance.CheckType.RandomInfluencedByAnATTR:
+                EditorGUILayout.PropertyField(attribute_prop, new GUIContent("attribute"));
+                break;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Packages/ie.itcarlow.turnbasedcombat/Tests/Runtime/FirstStrikeChanceTests.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class FirstStrikeChanceTests
+{
+    private GameObject m_gamePrefab;
+    [SetUp]
+    public void Setup()
+    {
+        m_gamePrefab = Resources.Load<GameObject>("Prefabs/CombatSystem");
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(m_gamePrefab);
+    }
+
+    [UnityTest]
+    public IEnumerator CalculateFirstStrikeChanceUsingRandomValAndAnATTRTest()
+    {
+        m_gamePrefab = Object.Instantiate(m_gamePrefab);
+        FirstStrikeChance script = m_gamePrefab.GetComponent<FirstStrikeChance>();
+        
+        script.SetType(FirstStrikeChance.CheckType.RandomInfluencedByAnATTR);
+        script.SetAttribute("Speed", 50);
+        float randomValSim = 10.0f; // simulate random value
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(script.FirstStrikeCheckTestVer(randomValSim), false);
+    }
+
+    [UnityTest]
+    public IEnumerator CalculateFirstStrikeChanceUsingBooleanExprTest()
+    {
+        m_gamePrefab = Object.Instantiate(m_gamePrefab);
+        FirstStrikeChance script = m_gamePrefab.GetComponent<FirstStrikeChance>();
+
+        script.SetType(FirstStrikeChance.CheckType.BooleanExpressions);
+        script.SetOnAdvantage(true);
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(script.FirstStrikeCheckTestVer(0.0f), true);
+    }
+}
