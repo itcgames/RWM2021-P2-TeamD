8117833d349ba4f66032ec88da80d53105916eb6
Szymon Piskorz <45246922+SzymonPiskorz@users.noreply.github.com>
2021-11-18 16:57:34 +0000

    Task one finished (#1)

---
+++ Assets/Scripts/ScreenSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenSystem : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void GoToCharacterSelcetionScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToGameplayScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ContinueGame()
+    {
+        //load saved info here
+        GoToGameplayScene();
+    }
+}
---
+++ Assets/Tests/ScreenSystemTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class ScreenSystemTest
+    {
+        [UnityTest]
+        public IEnumerator SwitchToCharacterSelection()
+        {
+            ScreenSystem t_system = new ScreenSystem();
+
+            t_system.GoToCharacterSelcetionScene();
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(1, SceneManager.GetActiveScene().buildIndex);
+        }
+
+        [UnityTest]
+        public IEnumerator SwitchToGame()
+        {
+            ScreenSystem t_system = new ScreenSystem();
+
+            t_system.GoToGameplayScene();
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(2, SceneManager.GetActiveScene().buildIndex);
+        }
+
+        [UnityTest]
+        public IEnumerator SwitchToGameFromContinue()
+        {
+            ScreenSystem t_system = new ScreenSystem();
+
+            t_system.ContinueGame();
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(2, SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
