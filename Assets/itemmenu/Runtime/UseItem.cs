86d768f1fd13392e2c536d507ac10c0c7555f9b4
Ben Stynes <C00239718@itcarlow.ie>
2021-11-20 20:52:48 +0000

    Item works using heal and menu set up

---
+++ Assets/itemmenu/Runtime/UseItem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UseItem : MonoBehaviour
+{
+    public GameObject Item;
+    public int amount;
+    public string itemName;
+    public int healAmount;
+    public int PlayerHealth;
+    public int max_health;
+    public string playername;
+   public Text playerHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+    private void OnEnable()
+    {
+        playerHP.text = playername + " \n HP" + PlayerHealth.ToString() + "/ " + max_health.ToString();
+      
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        amount = Item.GetComponent<setupItem>().getAmount();
+        Item.GetComponentInChildren<Text>().GetComponentInChildren<Text>().text = itemName +" x " + amount.ToString();
+        
+        if(amount == 0)
+        {
+            Destroy(Item);
+        }
+    }
+
+    public void heal()
+    {
+        if (PlayerHealth < max_health && PlayerHealth != 0)
+        {
+           
+            PlayerHealth += healAmount;
+        }
+        if(PlayerHealth > max_health)
+        {
+            PlayerHealth = max_health;
+        }
+        
+    }
+    public void use()
+    {
+        if (amount> 0 && PlayerHealth != 0)
+        {
+            Item.GetComponent<setupItem>().changeAmount(1);
+        }
+    }
+
+   
+}
---
+++ Assets/itemmenu/Runtime/setupItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setupItem : MonoBehaviour
+{
+    
+    public int itemAmount;
+    public string itemName;
+    private int amount;
+    public bool clicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clicked = false;
+        amount = itemAmount;
+        GetComponentInChildren<Text>().text = itemName;
+        GetComponentInChildren<Text>().GetComponentInChildren<Text>().text =  itemName +" x " + amount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void click()
+    {
+        clicked = !clicked;
+    }
+    public int getAmount()
+    {
+        return amount;
+    }
+
+   public void changeAmount(int t_amount)
+    {
+      amount =  amount - t_amount;
+    }
+}
