08f44cccb68c97002cb68e17965fd713a80cf258
Szymon Piskorz <45246922+SzymonPiskorz@users.noreply.github.com>
2021-11-22 10:34:48 +0000

    Feature Screen System Finished - RR-4 (#5)
    * Task one finished (#1)
    
    * Task two finished as much as possible (#2)
    
    * Task switch to inventory screens finished (#4)
    
    * Task partially finished
    
    * Task three finished

---
+++ Assets/Scripts/ScreenSystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenSystem : MonoBehaviour
+{
+    int m_currentInventory = 0;
+
+    private void Awake()
+    {
+        if (this.gameObject.name != "Canvas")
+            DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void GoToCharacterSelcetionScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToGameplayScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ContinueGame()
+    {
+        //load saved info here
+        GoToGameplayScene();
+    }
+
+    public void GoToPauseScreen()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToInventoryScreen(int t_i)
+    {
+        m_currentInventory = t_i;
+
+        if(this.gameObject != null)
+            if(this.gameObject.name == "Canvas")
+            {
+                for (int i = 0; i < this.transform.childCount; i++)
+                {
+                    this.transform.GetChild(i).gameObject.SetActive(false);
+                }
+                this.transform.GetChild(t_i).gameObject.SetActive(true);
+            }
+    }
+
+    public int GetCurrentInventory()
+    {
+        return m_currentInventory;
+    }
+}
