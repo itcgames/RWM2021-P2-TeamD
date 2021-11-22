bf5a27dfa2ed4c0911f16a54fcf15ba9fd963df7
AllexisAlvarico <c00242855@itcarlow.ie>
2021-11-22 02:43:08 +0000

    added the transitions

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -7,13 +7,21 @@ public class SceneChange : MonoBehaviour
 {
     [SerializeField]
     private int m_scene;
+    public Animator m_animator;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(m_scene);
-
+            StartCoroutine("LoadLevel");
         }
     }
+    IEnumerator LoadLevel()
+    {
+        m_animator.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(m_scene);
+    }
+
 }
