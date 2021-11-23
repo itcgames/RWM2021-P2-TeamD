a10ef6911ee9ed801406e5772d9cc52a69e70e8b
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2021-11-23 10:59:59 +0000

    Overworld (#6)
    * Saving point
    
    * fixed the error on my movement
    
    * fixed the scenes
    
    * added the combat chances trigger
    
    * added the transitions
    
    * finished task.1 and task.2
    
    * finished task.3 and task.4

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform m_followTransform;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.position =
+            new Vector3(m_followTransform.position.x, m_followTransform.position.y,
+            this.transform.position.z);
+    }
+}
