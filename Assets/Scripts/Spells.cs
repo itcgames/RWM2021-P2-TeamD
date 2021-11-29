1bfeb59ae89a9ffac28a5af13474f4ba020ab2ef
SzymonPiskorz <c00242808@itcarlow.ie>
2021-11-29 12:06:55 +0000

    added spell uses

--- Assets/Scripts/Spells.cs
+++ Assets/Scripts/Spells.cs
@@ -11,89 +11,53 @@ public enum SpellType
 public enum BlackSpells
 {
     //Black
-    Fire,
-    Thunder,
-    Focus,
-    Sleep,
-    Dark,
-    Blizzard,
-    Slow,
-    Temper,
-    Fira,
-    Hold,
-    Thundara,
-    Focara,
-    Confuse,
-    Haste,
-    Blizzara,
-    Sleepra,
-    Scourge,
-    Firaga,
-    Slowra,
-    Teleport,
-    Thundaga,
-    Quake,
-    Death,
-    Stun,
-    Blind,
-    Break,
-    Blizzaga,
-    Saber,
-    Flare,
-    Stop,
-    Kill,
-    Warp
+    Fire
 }
 
 public enum WhiteSpells
 {
     //White
-    Cure,
-    Protect,
-    Dia,
-    Blink,
-    NulShock,
-    Invis,
-    Blinda,
-    Silence,
-    NulBlaze,
-    Cura,
-    Heal,
-    Diara,
-    NulFrost,
-    Vox,
-    Fear,
-    Poisona,
-    Curaga,
-    Healara,
-    Diaga,
-    Life,
-    Exit,
-    Protera,
-    Invisira,
-    Stona,
-    NulDeath,
-    Curaja,
-    Healaga,
-    Diaja,
-    Holy,
-    Full_Life,
-    NulAll,
-    Dispel
+    Cure
 }
 
 public class Spells : MonoBehaviour
 {
-    
-    void AddSpell(SpellType t_type /* character type*/)
+    List<WhiteSpells> currentWhiteSpells;
+    List<BlackSpells> currentBlackSpells;
+    int spellUses = 0;
+    int MaxUses = 2;
+
+    public void AddSpell(SpellType t_type /* character type*/)
     {
         if(t_type.ToString() == "WhiteSpells")
         {
-
+            currentWhiteSpells.Add(WhiteSpells.Cure);
         }
         else
         {
-
+            currentBlackSpells.Add(BlackSpells.Fire);
         }
+    }
+
+    public int UsesLeft()
+    {
+        return spellUses; 
+    }
+
+    public void RechargeUses()
+    {
+        spellUses = MaxUses;
+    }
+
+    public void SetMaxUses(int t_increase)
+    {
+        MaxUses += t_increase;
+    }
+
+    public void useSpell()
+    {
+        spellUses--;
+        if (spellUses < 0)
+            spellUses = 0;
     }
 }
