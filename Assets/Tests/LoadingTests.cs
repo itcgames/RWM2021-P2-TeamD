66e894bc5f0c023d91918f6323ba5cdc1b0f478b
Szymon Piskorz <45246922+SzymonPiskorz@users.noreply.github.com>
2021-12-05 20:29:05 +0000

    Task one finished (#18)

---
+++ Assets/Tests/LoadingTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class LoadingTests
+    {
+        [UnityTest]
+        public IEnumerator CallLoadAfterContinue()
+        {
+            ScreenSystem t_system = new ScreenSystem();
+            t_system.GoToScene(0);
+
+            yield return new WaitForSeconds(0.1f);
+
+            t_system.ContinueGame();
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(2, SceneManager.GetActiveScene().buildIndex);
+            UnityEngine.Assertions.Assert.IsNotNull(GameObject.FindObjectOfType<PlayerAndGameInfo>().infos);
+        }
+    }
+}
