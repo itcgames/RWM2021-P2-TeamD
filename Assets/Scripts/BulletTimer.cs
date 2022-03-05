63e4419963bfba522d3cda1147077903ad814387
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-03-05 15:07:51 +0000

    added player shooting

---
+++ Assets/Scripts/BulletTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTimer : MonoBehaviour
+{
+    private float m_timeToDie = 4.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timeToDie -= Time.deltaTime;
+
+        if(m_timeToDie <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
