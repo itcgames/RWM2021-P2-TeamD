f7111a7114f3781cdfc0be0f8fbf44bee59d70d4
Szymon Piskorz <45246922+SzymonPiskorz@users.noreply.github.com>
2022-03-06 21:04:12 +0000

    Task [RR-63] finished (#32)

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            FindObjectOfType<ScreenSystem>().EndpointHit();
+    }
+}
---
+++ Assets/Tests/EndpointTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class EndpointTests
+    {
+
+        [UnityTest]
+        public IEnumerator LocationEndpointWorks()
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+
+            yield return new WaitForSeconds(0.1f);
+
+            ScreenSystem t_system = GameObject.FindObjectOfType<ScreenSystem>();
+
+            t_system.GoToGameplayScene();
+
+            yield return new WaitForSeconds(0.1f);
+
+            var endpoint = GameObject.Find("EndPoint");
+            var player = GameObject.Find("Player");
+            endpoint.transform.position = player.transform.position;
+
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.AreEqual(0, SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
