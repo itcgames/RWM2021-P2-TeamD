674033197a366169c240eb781803cefe0b815c84
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-03-06 19:38:21 +0000

    added enemy shooting, player shooting, & combat with advantage/disadvantage

--- Assets/Scripts/BulletTimer.cs
+++ Assets/Scripts/BulletTimer.cs
@@ -11,9 +11,34 @@ public class BulletTimer : MonoBehaviour
     {
         m_timeToDie -= Time.deltaTime;
 
-        if(m_timeToDie <= 0.0f)
+        if (m_timeToDie <= 0.0f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") && tag == "Player Bullet")
+        {
+            Debug.Log("You have encountered an enemy!");
+            CombatEnum.s_advantage = true;
+            // add scene for battle
+            GameObject.Find("SceneManager").GetComponent<ScreenSystem>().GoToCombatScene();
+        }
+
+        else if (other.CompareTag("Player") && tag == "Enemy Bullet")
+        {
+            Debug.Log("You have encountered an enemy!");
+            CombatEnum.s_advantage = false;
+            // add scene for battle
+
+            GameObject sceneManager = GameObject.Find("SceneManager");
+
+            if (sceneManager != null)
+            {
+                sceneManager.GetComponent<ScreenSystem>().GoToCombatScene();
+            }
         }
     }
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float initialShotDelay;
+    private float shotCooldown;
+    private bool m_readyToFire = false;
+    public GameObject m_bullet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCooldown = initialShotDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shotCooldown >= 0.0f)
+        {
+            shotCooldown -= Time.deltaTime;
+        }
+        else if (shotCooldown <= 0.0f && !m_readyToFire)
+        {
+            m_readyToFire = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (m_readyToFire)
+            {
+                Fire(collision.transform.position);
+            }
+        }
+
+    }
+
+    public void Fire(Vector2 target)
+    {
+        GameObject bullet = Instantiate(m_bullet, transform.position, Quaternion.identity);
+
+        Vector2 pos = transform.position;
+        Vector2 direction = (target - pos).normalized;
+
+        bullet.GetComponent<Rigidbody2D>().velocity = direction * 10.0f;
+
+        m_readyToFire = false;
+        shotCooldown = initialShotDelay;
     }
 }
