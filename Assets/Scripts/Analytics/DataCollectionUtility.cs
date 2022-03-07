4b6bdb306bf4132ada4e4e0c7f26007cf7679f45
Michał Krzyżanowski <45101664+MichalKrzyzanowski@users.noreply.github.com>
2022-03-07 11:31:57 +0000

    Finished send data task [RR-56] (#36)
    * added data collection code, untested since server is not ready yet
    
    * added data to sent

---
+++ Assets/Scripts/Analytics/DataCollectionUtility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameState
+{
+    public int id;
+}
+
+[System.Serializable]
+public class CombatData : GameState
+{
+    public int onAdvantage;
+    public int enemyCount;
+    public int turnTotal;
+    public int victory;
+}
+
+public class DataCollectionUtility : MonoBehaviour
+{
+    // monobehaviour passed in since you need an object to run a coroutine.
+    // just pass in "this" keywork when calling the func()
+    public static void PostData(GameState data, MonoBehaviour toRunCoroutine)
+    {
+        string jsonData = JsonUtility.ToJson(data);
+
+        toRunCoroutine.StartCoroutine(AnalyticsManager.PostMethod(jsonData));
+    }
+}
