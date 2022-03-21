0c8b317d9169633de681e8ccb1dd01bbcfd5a1fc
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2022-03-21 09:24:33 +0000

    [RR-96] collision (#63)
    * [RR-96] starting point
    
    * [RR-97] starting point
    
    * [RR-97] the river have collision with the player
    
    * Rr 97 river collision (#61)
    
    * [RR-97] starting point
    
    * [RR-97] the river have collision with the player
    
    * [RR-98] starting point and fix river's collision radius
    
    * [RR-98] boulder have collisions
    
    * Saving point
    
    * Changed the player movement to work with the colliders
    
    * changed the path
    
    * Revert "fixing the merge error"
    
    This reverts commit 02140e8e0d880c75b2e0c17f5c109bed86a8cec1, reversing
    changes made to 20eda764b358562f2071c1cba507afd2bdae0279.
    
    * minor change
    
    * started and finished unit-tests
    
    * changed the movement again

---
+++ Assets/Tests/CollisionTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class CollisionTest
+    {
+        private GameObject m_player;
+        private GameObject m_instance;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            SceneManager.LoadScene("OverworldScene", LoadSceneMode.Single);
+        }
+        // A Test behaves as an ordinary method
+
+        [UnityTest]
+        public IEnumerator RiverCollision()
+        {
+            m_player = GameObject.Find("Player");
+            m_player.transform.position = new Vector2(-4.9f, 1.4f);
+            m_player.transform.position += new Vector3(-0.5f, 0,0);
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(true, m_player.GetComponent<Player>().getCollisionCheck());
+        }
+
+        [UnityTest]
+        public IEnumerator BoulderCollision()
+        {
+            m_player = GameObject.Find("Player");
+            m_player.transform.position = new Vector2(-0.75f, 4.0f);
+            m_player.transform.position += new Vector3(0, 1f, 0);
+            yield return new WaitForSeconds(0.5f);
+            Assert.AreEqual(true,m_player.GetComponent<Player>().getCollisionCheck());
+        }
+
+    }
+}
