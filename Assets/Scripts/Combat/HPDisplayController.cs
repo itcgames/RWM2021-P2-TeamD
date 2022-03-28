3aac6894cd43210dd4f908bb799302ceedab95d9
MichalKrzyzanowski <c00240696@itcarlow.ie>
2022-03-28 22:41:39 +0100

    backup, working on changing hp bars

--- Assets/Scripts/Combat/HPDisplayController.cs
+++ Assets/Scripts/Combat/HPDisplayController.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class HPDisplayController : MonoBehaviour
 {
+    public float InitialHp; // 100% of enemy's hp
+    private float HpReduction; // % based
+    private float HpScale; // x axis scale
+    private float currentHp;
+
+    private EnemySelector m_parentScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_parentScript = transform.parent.GetComponent<EnemySelector>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_parentScript.GetEnemyHP(ref currentHp, ref InitialHp);
+
+        transform.GetChild(0).transform.localScale = new Vector2(transform.GetChild(0).transform.localScale.x - HpReduction,
+            transform.GetChild(0).transform.localScale.y);
+    }
+
+    void CalculateReduction(float currentHp)
+    {
+        float reduction = (InitialHp - currentHp) / InitialHp * 100;
+
+        HpReduction = (transform.GetChild(0).transform.localScale.x / 100 * reduction);
     }
 }
