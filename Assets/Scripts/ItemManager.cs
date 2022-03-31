d3ee697afbcbb4e68aac7ab46242d7acaf5bd0f4
Allexis Alvarico <44900809+AllexisAlvarico@users.noreply.github.com>
2022-03-31 17:00:45 +0100

    [RR-76] task finished

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
@@ -9,11 +10,12 @@ public class ItemManager : MonoBehaviour
     public static ItemManager instance;
 
     public ItemInventory[] items;
+    public GameObject buttonPrefab;
+    GameObject itemCanvas;
 
 
     private void Awake()
     {
-
         if (instance == null)
             instance = this;
         else
@@ -21,17 +23,24 @@ public class ItemManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
 
 
     public void setAllItems()
     {
+        itemCanvas = GameObject.Find("Itemarea");
+
         foreach(ItemInventory i in items)
         {
-
-
-
+            if(i.m_amount > 0)
+            {
+                GameObject newButton = Instantiate(buttonPrefab) as GameObject;
+                newButton.transform.SetParent(itemCanvas.transform, false);
+                newButton.GetComponent<Text>().text = i.m_name + " x" + i.m_amount;
+            }
         }
     }
 
@@ -45,6 +54,19 @@ public class ItemManager : MonoBehaviour
             return;
         }
         itemAmount.m_amount++;
+
+        print(name + ": " + itemAmount.m_amount);
+    }
+
+    public void UseItem(string name)
+    {
+        ItemInventory itemAmount = Array.Find(items, items => items.m_name == name);
+        if (itemAmount == null)
+        {
+            Debug.LogWarning("Item: " + name + " not found!");
+            return;
+        }
+        itemAmount.m_amount--;
 
         print(name + ": " + itemAmount.m_amount);
     }
