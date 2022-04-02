f8e3885ae486df74248775ff67b9d1a5e5a0067f
SzymonPiskorz <c00242808@itcarlow.ie>
2022-04-02 22:49:20 +0100

    Fixed a few tests

--- Assets/Tests/useItemTest.cs
+++ Assets/Tests/useItemTest.cs
@@ -10,26 +10,16 @@ namespace Tests
     public class usetemTest
     {
         
-        
-        GameObject testItem;
-        [SetUp]
-        public void SetUp()
-        {
-            SceneManager.LoadScene("itemScene");
-            
-        }
-       [TearDown]
-       public void TearDown()
-        {
-            SceneManager.UnloadSceneAsync("itemScene");
-        }
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
         public IEnumerator SetupAmount()
         {
-            GameObject itemTest = GameObject.Find("item");
-            int expected = itemTest.GetComponent<setupItem>().getAmount();
+            SceneManager.LoadScene(6);
+            yield return new WaitForSeconds(0.1f);
+
+            setupItem itemTest = GameObject.FindObjectOfType<setupItem>();
+            int expected = itemTest.getAmount();
             Assert.AreEqual(expected, 55);
             yield return null;
 
@@ -37,14 +27,26 @@ namespace Tests
         [UnityTest]
         public IEnumerator UseAmount()
         {
+            SceneManager.LoadScene(6);
+            yield return new WaitForSeconds(0.1f);
 
-            GameObject camera = GameObject.Find("inven");
+            setupItem itemTest = GameObject.FindObjectOfType<setupItem>();
+
+            ActivateAndDeactivate camera = GameObject.FindObjectOfType<ActivateAndDeactivate>();
+
+            yield return new WaitForSeconds(0.1f);
+
             camera.GetComponent<ActivateAndDeactivate>().ChangeInventory(true);
             camera.GetComponent<ActivateAndDeactivate>().ChangeUse(true);
-            GameObject playerTest = GameObject.Find("playerData");
-            GameObject itemTest = GameObject.Find("item");
-            playerTest.GetComponent<UseItem>().use();
-            int expected = itemTest.GetComponent<setupItem>().getAmount();
+
+            yield return new WaitForSeconds(0.1f);
+
+            UseItem playerTest = GameObject.FindObjectOfType<UseItem>();
+
+            yield return new WaitForSeconds(0.1f);
+
+            playerTest.use();
+            int expected = itemTest.getAmount();
 
 
             yield return null;
@@ -55,6 +57,8 @@ namespace Tests
         [UnityTest]
         public IEnumerator Healamount()
         {
+            SceneManager.LoadScene(6);
+            yield return new WaitForSeconds(0.1f);
 
             GameObject camera = GameObject.Find("inven");
             camera.GetComponent<ActivateAndDeactivate>().ChangeInventory(true);
